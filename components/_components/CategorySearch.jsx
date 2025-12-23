27318f32f6c9028744949f01efbbb69249f71a40
zaynab-abdallah <zaynabshaltout718@gmail.com>
2025-12-23 21:56:29 +0200

    Update CategorySearch.jsx

--- components/_components/CategorySearch.jsx
+++ components/_components/CategorySearch.jsx
@@ -72,7 +72,7 @@ function CategorySearch() {
 
 
       {/* Doctors */}
-      <div className="grid grid-cols-1 justify-items-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="grid grid-cols-1 justify-items-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         {filteredDoctors.length > 0 ? (
           filteredDoctors.map((doctor) => (
             <DoctorCard key={doctor.id} doctor={doctor} />
